1971a6ed2e963b5bf50f5ea8561bb7e20455ec84
Martin Martin <morskijk@gmail.com>
2025-11-19 08:52:11 +0200

    init commit

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <a className="home" href="#"> <img src="./images/logo.png" alt="logo" /> </a>
+                <a href="#">Catalog</a>
+                <div id="user">
+                    <a href="#">Add Game</a>
+                    <a href="#">Logout</a>
+                </div>
+
+                <div id="guest">
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                </div>
+            </nav>
+        </header>
+    );
+}
